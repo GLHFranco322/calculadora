e971e18297378170b1ab75ddd44ec11bf6f9e050
Alexandro Lucero <alexandro71000@gmail.com>
2024-10-24 20:26:38 -0300

    corrigiendo algo

--- suma.js
+++ suma.js
@@ -1,12 +1,12 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
-function sumar() {
+function suma() {
     let num1 = parseFloat(prompt("Ingrese el primer numero: "))
     let num2 = parseFloat(prompt("Ingrese el segundo numero: "))
     let resultado = num1 + num2
     console.log(`El resultado de la suma es: ${resultado}`)
 }
-sumar()
+suma()
 
 // Bloque para que el usuario elija que quiere hacer
 
