7719d18e7df781788dc89a4bfe87aa27a4a2fdf8
Alexandro Lucero <alexandro71000@gmail.com>
2024-10-24 20:34:44 -0300

    corrigiendo

--- suma.js
+++ suma.js
@@ -1,12 +1,15 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
+function sumar(a, b) {;  //suma las 2 varibles, exporta la funcion
+    return a + b;
+}
+
 function suma() {
     let num1 = parseFloat(prompt("Ingrese el primer numero: "))
     let num2 = parseFloat(prompt("Ingrese el segundo numero: "))
-    let resultado = num1 + num2
+    let resultado = sumar(num1, num2)
     console.log(`El resultado de la suma es: ${resultado}`)
 }
-suma()
+suma() //llamar a la funcion
 
-// Bloque para que el usuario elija que quiere hacer
-
+module.exports = suma  //para exportar
