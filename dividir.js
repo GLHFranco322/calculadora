4460073f420c7a369200a6f1ccbea0a336a77862
Franco Spiazzi <francospiazzi96@gmail.com>
2024-10-24 20:36:21 -0300

    cambie la rama a dividir

--- dividir.js
+++ dividir.js
@@ -1,20 +1,24 @@
-const prompt = require("prompt-sync")({ sigint: true });
+const prompt = require('prompt-sync')({ sigint: true });
 
-let num1 = parseInt(prompt("Por favor, introduce el primer número:"));
-let num2 = parseInt(prompt("Por favor, introduce el segundo número:"));
+function dividir() {
+    let num1 = parseInt(prompt("Por favor, introduce el primer número:"), 10);
+    let num2 = parseInt(prompt("Por favor, introduce el segundo número:"), 10);
 
-switch (true) {
-    case isNaN(num1):
-        console.log("El primer número no es válido.");
-        break;
-    case isNaN(num2):
-        console.log("El segundo número no es válido.");
-        break;
-    case num2 === 0:
-        console.log("El divisor no puede ser cero.");
-        break;
-    default:
-        let result = num1 / num2;
-        console.log("El resultado de la división es: " + result);
-        break;
+    switch (true) {
+        case isNaN(num1):
+            console.log("El primer número no es válido.");
+            break;
+        case isNaN(num2):
+            console.log("El segundo número no es válido.");
+            break;
+        case num2 === 0:
+            console.log("El divisor no puede ser cero.");
+            break;
+        default:
+            let result = num1 / num2;
+            console.log("El resultado de la división es: " + result);
+            break;
+    }
 }
+
+module.exports = dividir;
